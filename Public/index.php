afe147997f9abe241586d0225994f3b51f097d95
stephane <stephanesonkin7@gmail.com>
2022-08-22 21:08:01 +0100

    Ajout du formulaire de connexion

--- Public/index.php
+++ Public/index.php
@@ -19,32 +19,3 @@ $app = new Main();
 
 //ondemarre l'application dans le routeur (Main)
 $app->start();
-
-
-
-
-
-
-// $model = new UsersModel;
-
-// $user = $model->setEmail('stephanesonkin7@gmail.com')
-//     ->setPassword(password_hash('azerty', PASSWORD_ARGON2I));
-
-
-// $model->create($user);
-
-// var_dump($model);
-
-// $donnees = [
-//     'titre' => 'Annonces modifiée',
-//     'description' => 'Description de l\'annonce modifiée',
-//     'actif' => 1
-// ];
-
-// $annonces = $model->hydrate($donnees);
-
-// $model->delete(5);
-
-// var_dump($annonces); 
-
-// var_dump($model->findAll());
---
+++ Views/users/login.php
@@ -0,0 +1,2 @@
+<h1>CONNEXION</h1>
+<?= $loginForm ?>
